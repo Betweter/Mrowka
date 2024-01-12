dd3d75323f623052819c2004fc138ae464d663fa
GT <tesla.grzes@gmail.com>
2024-01-12 10:35:06 +0100

    pozpion

--- src/Langton.c
+++ src/Langton.c
@@ -88,8 +88,8 @@ int kier_zmian(char kierunek_poczatkowy){
 
 int Langton(int wiersze, int kolumny,int  l_iteracji, char kierunek_poczatkowy, char* przedrostek_pliku, int czy_losowo, double procent_przeszkod, char* mapa ){
 	
-	int x = wiersze/2;
-	int y = kolumny/2;
+	int x = kolumny/2;
+	int y = wiersze/2;
 
 	int* plansza = mapa == NULL ? calloc(wiersze*kolumny, sizeof(plansza)) : wczytaj(mapa, &wiersze, &kolumny, &kierunek_poczatkowy, &x, &y);
 	int kierunek = kier_zmian(kierunek_poczatkowy);
